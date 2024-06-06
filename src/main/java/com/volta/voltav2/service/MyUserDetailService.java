38b538b16af6680d39d6576da03c004e5ab2b2f9
Mourya <varma.mourya1@gmail.com>
2024-06-06 13:33:49 -0500

    Added token utility.

---
+++ src/main/java/com/volta/voltav2/service/MyUserDetailService.java
@@ -0,0 +1,28 @@
+package com.volta.voltav2.service;
+
+import com.volta.voltav2.model.User;
+import com.volta.voltav2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    public UserDetails loadUserByUseID(Long id) throws UsernameNotFoundException {
+      User user = userRepository.findById(id).orElseThrow(()-> new UsernameNotFoundException("User not found"));;
+      return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByusername(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));;
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
